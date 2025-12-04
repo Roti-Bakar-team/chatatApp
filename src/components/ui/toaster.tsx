d614fcf91742d519d9674ab7740aa0b227c5db16
arnolis05 <arnolis321@gmail.com>
2025-12-05 01:52:51 +0700

    Update component

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,6 +1,5 @@
 "use client" // harus paling atas
 
-import React from "react"
 import { useToast } from "@/hooks/use-toast"
 import {
   Toast,
@@ -12,32 +11,17 @@ import {
 } from "@/components/ui/toast"
 
 export function Toaster() {
-  const { toasts, removeToast } = useToast()
+  const { toasts } = useToast() // ambil array toasts dari custom hook
 
   return (
     <ToastProvider>
-<<<<<<< HEAD
       {toasts.map(({ id, title, description, action, ...props }) => (
-        <Toast
-          key={id}
-          open={true} // toast selalu muncul saat ada di array
-          onOpenChange={(open) => {
-            if (!open) removeToast(id)
-          }}
-          {...props}
-        >
-=======
-      {toasts.map(({ id, title, description, ...props }) => (
         <Toast key={id} {...props}>
->>>>>>> dev
           <div className="grid gap-1">
             {title && <ToastTitle>{title}</ToastTitle>}
             {description && <ToastDescription>{description}</ToastDescription>}
           </div>
-<<<<<<< HEAD
           {action}
-=======
->>>>>>> dev
           <ToastClose />
         </Toast>
       ))}
