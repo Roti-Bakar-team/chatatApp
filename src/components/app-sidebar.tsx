019fea7b78a39bbc47a12366b72b517eb9d38dfb
Hadiid Andri Yulison <hadiidandri2000@gmail.com>
2025-12-07 15:35:09 +0700

    sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -4,24 +4,15 @@ import * as React from "react";
 import {
   IconBuildingStore,
   IconCamera,
-  IconChartBar,
   IconDashboard,
-  IconDatabase,
   IconFileAi,
   IconFileDescription,
-  IconFileWord,
-  IconFolder,
   IconHelp,
   IconInnerShadowTop,
-  IconListDetails,
-  IconReport,
-  IconSearch,
   IconSettings,
-  IconUsers,
   IconUsersGroup,
 } from "@tabler/icons-react";
 
-import { NavDocuments } from "@/components/nav-documents";
 import { NavMain } from "@/components/nav-main";
 import { NavSecondary } from "@/components/nav-secondary";
 import { NavUser } from "@/components/nav-user";
